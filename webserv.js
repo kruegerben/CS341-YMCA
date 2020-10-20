46da1644da4fcee9ab3479947abe4c679aee8484
kruegerben <krueger.benjamin21@gmail.com>
2020-10-19 20:03:38 -0500

    Framework

---
+++ webserv.js
@@ -0,0 +1,11 @@
+var express = require('express');
+const livereload = require('livereload');
+const reload = livereload.createServer();
+reload.watch(__dirname);
+var port = 8080;
+var app = express();
+const sqlite3 = require('sqlite3').verbose();
+
+app.get('/', function(req, res) {
+    
+})
