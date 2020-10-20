e414cc79298ef2501371fd9bf55986430d32b5da
kruegerben <krueger.benjamin21@gmail.com>
2020-10-20 14:43:43 -0500

    Reset

--- webserv.js
+++ webserv.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const livereload = require('livereload');
 const reload = livereload.createServer();
+const prompt = require('prompt');
 reload.watch(__dirname);
 var port = 8080;
 var app = express();
@@ -10,7 +11,11 @@ let db = new sqlite3.Database('./CS341-YMCA.db');
 
 app.get('/', function(req, res) {
     console.log('Connection made')
-  });
+});
+
+app.post('/', function(req, res) {
+
+});
 
 app.listen(port, function() {
     console.log('Listening on port %d', port);
