059bd0496ae28bad696cd72f5b02ce5e116f7c35
kruegerben <krueger.benjamin21@gmail.com>
2020-10-19 21:06:18 -0500

    Finished framework

--- webserv.js
+++ webserv.js
@@ -7,5 +7,9 @@ var app = express();
 const sqlite3 = require('sqlite3').verbose();
 
 app.get('/', function(req, res) {
-    
+    console.log('Connection made')
+  });
+
+app.listen(port, function() {
+    console.log('Listening on port %d', port);
 })
